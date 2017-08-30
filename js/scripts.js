eae87ca9af2d383ab6afa5285bbb8f7295daafc8
Michał <michal.bootcamp@gmail.com>
2017-08-30 14:49:12 +0200

    poprawa wyswietlania zmiennych

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-// scripts.js
-
-var a = prompt('Wpisz liczbę \' a\'');
-var b = prompt('Wpisz liczbę\' b\'');
+var a = prompt('Wpisz liczbę \'a\'');
+var b = prompt('Wpisz liczbę \'b\'');
 var value = (a*a) + (2*a*b) - (b*b);
 
 console.log(value);
